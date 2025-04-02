03194284b79e0202e94bf89522e666fe1582a32d
hareesh <hareesh.puliyedath@gmail.com>
2025-04-02 00:50:44 -0700

    add badge colors

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -1,7 +1,16 @@
 export default function Badge({ source }: { source: string }) {
+  const colorMap: Record<string, string> = {
+    NETFLIX: "bg-[#e50914]",
+    AMAZON: "bg-[blue]",
+    HULU: "bg-[green]",
+    "DISNEY PLUS": "bg-[#85a7c3]",
+    "PRIME VIDEO": "bg-[#79b8f3]",
+  };
   return (
     <div className="mt-3 ">
-      <span className="inline-block px-2 py-1 text-xs font-semibold rounded bg-[#8B4513] text-white">
+      <span
+        className={`inline-block px-2 py-1 text-xs font-semibold rounded ${colorMap[source]} text-white`}
+      >
         {source}
       </span>
     </div>
