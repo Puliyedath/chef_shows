08a76f6cf5e492f625cb6d3069bd73b7c15c70fe
hareesh <hareesh.puliyedath@gmail.com>
2025-04-01 23:02:56 -0700

    add a calendar svg for the year

--- app/components/Metadata.tsx
+++ app/components/Metadata.tsx
@@ -1,8 +1,19 @@
 export default function Metadata({ age, year }: { age: number; year: number }) {
   return (
-    <div className="flex items-center gap-2 text-sm text-gray-700">
-      <span>{year}</span>
-      <span>•</span>
+    <div className="flex items-center justify-between gap-2 text-sm text-gray-700">
+      <span className="flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          className="bi bi-calendar"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3.5 0a.5.5 0 0 1 .5.5V1h9V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H1a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+        </svg>
+        {year}
+      </span>
       <span>{age}+</span>
     </div>
   );
